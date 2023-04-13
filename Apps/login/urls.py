e02bd67f961e7a111c95c8467af076c12926f5dd
DianaDeLeon <ddeleonr9@miumg.edu.gt>
2023-04-12 22:13:03 -0600

    Carga de templates

---
+++ Apps/login/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for ColegioG3 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Apps.login import views
+from .views import LoginView,HomeView,PrimeraUnidadView
+
+app_name='login'
+
+urlpatterns = [
+    path('', LoginView.as_view(), name='loginapp'),
+    path('home/', HomeView.as_view(), name='homeapp'),
+    path('primeraUnidad/', PrimeraUnidadView.as_view(), name='primeraunidadapp'),
+]
---
+++ Apps/login/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+class LoginView(TemplateView):
+    template_name='index.html'
+
+class HomeView(TemplateView):
+    template_name='home.html'
+
+class PrimeraUnidadView(TemplateView):
+    template_name='PrimeraUnidad/listado1unidad.html'
