60c58e92c77004bd084c5b3fc1ebbb598a006da5
SaraJru <sarajr_2.0@hotmail.com>
2023-05-23 23:36:13 -0600

    Delete middleware

--- ColegioG3/settings.py
+++ ColegioG3/settings.py
@@ -54,8 +54,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'ColegioG3.urls'
