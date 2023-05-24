55eabc1d1cfd66117992ea67aa2578bab20bff68
SaraJru <sarajr_2.0@hotmail.com>
2023-05-23 23:28:40 -0600

    config database connection

--- ColegioG3/settings.py
+++ ColegioG3/settings.py
@@ -85,7 +85,10 @@ WSGI_APPLICATION = 'ColegioG3.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
+    }
 }
 
 
