7312390543d8c7c5120dc5b47372dca6845de410
SaraJru <sarajr_2.0@hotmail.com>
2023-05-23 23:33:25 -0600

    Deleting Heroku files and config

--- ColegioG3/wsgi.py
+++ ColegioG3/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ColegioG3.settings')
 
 application = get_wsgi_application()
-
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
